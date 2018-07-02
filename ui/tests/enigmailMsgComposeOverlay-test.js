baf6f775dba760c5f26ea9aa7ca684043cea8c0c
Khushil Mistry <khushil324@gmail.com>
2018-07-03 03:17:35 +0530

    enigmailMsgComposeOverlay-test.js processFinalState test Added. gSMFields will be done with getAccDefault.

--- ui/tests/enigmailMsgComposeOverlay-test.js
+++ ui/tests/enigmailMsgComposeOverlay-test.js
@@ -1,12 +1,15 @@
-/* global Enigmail: false, Assert: false, do_load_module: false, trustAllKeys_test: false, JSUnit: false */
+/*global Enigmail: false, Assert: false, do_load_module: false, trustAllKeys_test: false, JSUnit: false, Components: false, EnigmailConstants: false, EnigmailLocale: false */
 /*
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
 
+
 var window;
 var document;
+
+var gSMFields = false;
 
 function trustAllKeys_test() {
   // test functionality of trustAllKeys
@@ -17,6 +20,211 @@ function trustAllKeys_test() {
   Enigmail.msg.tempTrustAllKeys();
   Assert.equal(Enigmail.msg.trustAllKeys, true, "check trustAllKeys is true");
 
+}
+
+function processFinalState_test() {
+  // Encryption Status and Reason
+
+  Enigmail.msg.isEnigmailEnabled = () => {
+      //Function Overriding
+      return false;
+  };
+
+  Enigmail.msg.isSmimeEnabled = () => {
+    //Function Overriding
+    return false;
+  };
+
+  Enigmail.msg.getAccDefault = (prop) => {
+    //Function Overriding
+    if(prop === "signIfEnc" || prop === "signIfNotEnc" || prop === "signIfNotEnc" || prop === "signIfEnc" || prop === "sign-pgp" || prop === "encrypt"){
+      return true;
+    }
+    else {
+      return false;
+    }
+  };
+
+  // Testing Encryption Flags
+
+  //Encryption reasonManuallyForced
+  Enigmail.msg.encryptForced = EnigmailConstants.ENIG_NEVER;
+  Enigmail.msg.processFinalState();
+  Assert.equal(Enigmail.msg.statusEncrypted, EnigmailConstants.ENIG_FINAL_FORCENO);
+  Assert.equal(Enigmail.msg.reasonEncrypted, EnigmailLocale.getString("reasonManuallyForced"));
+
+  //Encryption reasonManuallyForced
+  Enigmail.msg.encryptForced = EnigmailConstants.ENIG_ALWAYS;
+  Enigmail.msg.processFinalState();
+  Assert.equal(Enigmail.msg.statusEncrypted, EnigmailConstants.ENIG_FINAL_FORCEYES);
+  Assert.equal(Enigmail.msg.reasonEncrypted, EnigmailLocale.getString("reasonManuallyForced"));
+
+  //Encryption reasonByRecipientRules
+  Enigmail.msg.encryptForced = null;
+  Enigmail.msg.encryptByRules = EnigmailConstants.ENIG_NEVER;
+  Enigmail.msg.processFinalState();
+  Assert.equal(Enigmail.msg.statusEncrypted, EnigmailConstants.ENIG_FINAL_NO);
+  Assert.equal(Enigmail.msg.reasonEncrypted, EnigmailLocale.getString("reasonByRecipientRules"));
+
+  //Encryption reasonEnabledByDefault
+  Enigmail.msg.encryptByRules =  EnigmailConstants.ENIG_UNDEF;
+  Enigmail.msg.sendMode = 0x0002;
+  Enigmail.msg.isEnigmailEnabled = () => {
+    //Function Overriding
+    return true;
+  };
+  Enigmail.msg.processFinalState();
+  Assert.equal(Enigmail.msg.statusEncrypted,  EnigmailConstants.ENIG_FINAL_YES);
+  Assert.equal(Enigmail.msg.reasonEncrypted, EnigmailLocale.getString("reasonEnabledByDefault"));
+
+  //Encryption reasonEmpty
+  Enigmail.msg.encryptByRules =  EnigmailConstants.ENIG_UNDEF;
+  Enigmail.msg.sendMode = 0x0001;
+  Enigmail.msg.processFinalState();
+  Assert.equal(Enigmail.msg.statusEncrypted, EnigmailConstants.ENIG_FINAL_NO);
+  Assert.equal(Enigmail.msg.reasonEncrypted, "");
+
+  //Encryption reasonByRecipientRules
+  Enigmail.msg.encryptByRules = EnigmailConstants.ENIG_ALWAYS;
+  Enigmail.msg.processFinalState();
+  Assert.equal(Enigmail.msg.statusEncrypted, EnigmailConstants.ENIG_FINAL_YES);
+  Assert.equal(Enigmail.msg.reasonEncrypted, EnigmailLocale.getString("reasonByRecipientRules"));
+
+  //Encryption reasonByAutoEncryption
+  Enigmail.msg.encryptByRules = EnigmailConstants.ENIG_AUTO_ALWAYS;
+  Enigmail.msg.processFinalState();
+  Assert.equal(Enigmail.msg.statusEncrypted, EnigmailConstants.ENIG_FINAL_YES);
+  Assert.equal(Enigmail.msg.reasonEncrypted, EnigmailLocale.getString("reasonByAutoEncryption"));
+
+  //Encryption reasonByConflict
+  Enigmail.msg.encryptByRules = EnigmailConstants.ENIG_CONFLICT;
+  Enigmail.msg.processFinalState();
+  Assert.equal(Enigmail.msg.statusEncrypted, EnigmailConstants.ENIG_FINAL_CONFLICT);
+  Assert.equal(Enigmail.msg.reasonEncrypted, EnigmailLocale.getString("reasonByConflict"));
+
+  //Signing of Key
+
+  //Signing reasonManuallyForced
+  Enigmail.msg.signForced = EnigmailConstants.ENIG_NEVER;
+  Enigmail.msg.processFinalState();
+  Assert.equal(Enigmail.msg.statusSigned, EnigmailConstants.ENIG_FINAL_FORCENO);
+  Assert.equal(Enigmail.msg.reasonSigned, EnigmailLocale.getString("reasonManuallyForced"));
+
+  //Signing reasonManuallyForced
+  Enigmail.msg.signForced = EnigmailConstants.ENIG_ALWAYS;
+  Enigmail.msg.processFinalState();
+  Assert.equal(Enigmail.msg.statusSigned, EnigmailConstants.ENIG_FINAL_FORCEYES);
+  Assert.equal(Enigmail.msg.reasonSigned, EnigmailLocale.getString("reasonManuallyForced"));
+
+  //Signing reasonByRecipientRules
+  Enigmail.msg.signForced = null;
+  Enigmail.msg.signByRules = EnigmailConstants.ENIG_NEVER;
+  Enigmail.msg.processFinalState();
+  Assert.equal(Enigmail.msg.statusSigned, EnigmailConstants.ENIG_FINAL_NO);
+  Assert.equal(Enigmail.msg.reasonSigned, EnigmailLocale.getString("reasonByRecipientRules"));
+
+  //Signing reasonEnabledByDefault
+  Enigmail.msg.signByRules =  EnigmailConstants.ENIG_UNDEF;
+  Enigmail.msg.sendMode = 0x0001;
+  Enigmail.msg.finalSignDependsOnEncrypt = false;
+  Enigmail.msg.isEnigmailEnabled = () => {
+    //Function Overriding
+    return true;
+  };
+  Enigmail.msg.getAccDefault = () => {
+    //Function Overriding
+    return true;
+  };
+  Enigmail.msg.processFinalState();
+  Assert.equal(Enigmail.msg.statusSigned,  EnigmailConstants.ENIG_FINAL_YES);
+  Assert.equal(Enigmail.msg.reasonSigned, EnigmailLocale.getString("reasonEnabledByDefault"));
+
+  //Signing reasonEmpty
+  Enigmail.msg.signByRules =  EnigmailConstants.ENIG_UNDEF;
+  Enigmail.msg.sendMode = 0x0002;
+  Enigmail.msg.processFinalState();
+  Assert.equal(Enigmail.msg.statusSigned, EnigmailConstants.ENIG_FINAL_NO);
+  Assert.equal(Enigmail.msg.reasonSigned, "");
+
+  //Signing reasonByRecipientRules
+  Enigmail.msg.signByRules = EnigmailConstants.ENIG_ALWAYS;
+  Enigmail.msg.processFinalState();
+  Assert.equal(Enigmail.msg.statusSigned, EnigmailConstants.ENIG_FINAL_YES);
+  Assert.equal(Enigmail.msg.reasonSigned, EnigmailLocale.getString("reasonByRecipientRules"));
+
+  //Signing reasonByConflict
+  Enigmail.msg.signByRules = EnigmailConstants.ENIG_CONFLICT;
+  Enigmail.msg.processFinalState();
+  Assert.equal(Enigmail.msg.statusSigned, EnigmailConstants.ENIG_FINAL_CONFLICT);
+  Assert.equal(Enigmail.msg.reasonSigned, EnigmailLocale.getString("reasonByConflict"));
+
+  //finalSignDependsOnEncrypt Cases
+
+  //Encryption ENIG_ALWAYS
+  Enigmail.msg.isEnigmailEnabled = () => {
+    return true;
+  };
+  Enigmail.msg.signByRules =  EnigmailConstants.ENIG_UNDEF;
+  Enigmail.msg.encryptForced = EnigmailConstants.ENIG_ALWAYS;
+  Enigmail.msg.finalSignDependsOnEncrypt = true;
+  Enigmail.msg.processFinalState();
+  Assert.equal(Enigmail.msg.statusSigned, EnigmailConstants.ENIG_FINAL_YES);
+  Assert.equal(Enigmail.msg.reasonSigned, EnigmailLocale.getString("reasonByEncryptionMode"));
+
+  //Encryption ENIG_NEVER
+  Enigmail.msg.encryptForced = EnigmailConstants.ENIG_NEVER;
+  Enigmail.msg.processFinalState();
+  Assert.equal(Enigmail.msg.statusSigned, EnigmailConstants.ENIG_FINAL_YES);
+  Assert.equal(Enigmail.msg.reasonSigned, EnigmailLocale.getString("reasonByEncryptionMode"));
+
+  //Encryption encFinally = EnigmailConstants;
+  Enigmail.msg.encryptForced = null;
+  Enigmail.msg.encryptByRules = EnigmailConstants.ENIG_CONFLICT;
+  Enigmail.msg.processFinalState();
+  Assert.equal(Enigmail.msg.statusSigned, EnigmailConstants.ENIG_FINAL_YES);
+  Assert.equal(Enigmail.msg.reasonSigned, EnigmailLocale.getString("reasonByEncryptionMode"));
+
+  //Encryption ENIG_CONFLICT
+  Enigmail.msg.getAccDefault = (prop) => {
+    //Function Overriding
+    return false;
+  };
+  Enigmail.msg.sendMode = 0x0001;
+  Enigmail.msg.processFinalState();
+  Assert.equal(Enigmail.msg.statusSigned, EnigmailConstants.ENIG_FINAL_CONFLICT);
+  Assert.equal(Enigmail.msg.reasonSigned, "");
+
+  //statusPGPMime Flags
+
+  Enigmail.msg.pgpmimeForced = EnigmailConstants.ENIG_NEVER;
+  Enigmail.msg.processFinalState();
+  Assert.equal(Enigmail.msg.statusPGPMime, EnigmailConstants.ENIG_FINAL_FORCENO);
+
+  Enigmail.msg.pgpmimeForced = EnigmailConstants.ENIG_ALWAYS;
+  Enigmail.msg.processFinalState();
+  Assert.equal(Enigmail.msg.statusPGPMime, EnigmailConstants.ENIG_FINAL_FORCEYES);
+
+  Enigmail.msg.pgpmimeForced = "";
+  Enigmail.msg.pgpmimeByRules = EnigmailConstants.ENIG_NEVER;
+  Enigmail.msg.processFinalState();
+  Assert.equal(Enigmail.msg.statusPGPMime, EnigmailConstants.ENIG_FINAL_NO);
+
+  Enigmail.msg.pgpmimeByRules = EnigmailConstants.ENIG_ALWAYS;
+  Enigmail.msg.processFinalState();
+  Assert.equal(Enigmail.msg.statusPGPMime, EnigmailConstants.ENIG_FINAL_YES);
+
+  Enigmail.msg.pgpmimeByRules = EnigmailConstants.ENIG_CONFLICT;
+  Enigmail.msg.processFinalState();
+  Assert.equal(Enigmail.msg.statusPGPMime, EnigmailConstants.ENIG_FINAL_CONFLICT);
+
+  Enigmail.msg.pgpmimeByRules = EnigmailConstants.ENIG_UNDEF;
+  Enigmail.msg.processFinalState();
+  Assert.equal(Enigmail.msg.statusPGPMime, EnigmailConstants.ENIG_FINAL_NO);
+
+  Enigmail.msg.pgpmimeByRules = EnigmailConstants.ENIG_UNDEF;
+  Enigmail.msg.sendMode = EnigmailConstants.SEND_PGP_MIME;
+  Enigmail.msg.processFinalState();
+  Assert.equal(Enigmail.msg.statusPGPMime, EnigmailConstants.ENIG_FINAL_YES);
 
 }
 
@@ -26,6 +234,9 @@ function run_test() {
   document = window.document;
 
   do_load_module("chrome://enigmail/content/ui/enigmailMsgComposeOverlay.js");
+  do_load_module("chrome://enigmail/content/modules/constants.jsm");
+  do_load_module("chrome://enigmail/content/modules/locale.jsm");
 
   trustAllKeys_test();
+  processFinalState_test();
 }
